902d70cbcb5582fbeea7adccbeb489a4f53ed74c
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-16 15:58:47 -0300

    feat: add pagination and simulated user data

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,14 +6,47 @@ import {
   MoreHorizontal,
   Search,
 } from "lucide-react";
+import { ChangeEvent, useState } from "react";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { TableRow } from "./table/table-row";
+import { attendees } from "../data/attendees";
+
+dayjs.extend(relativeTime);
+dayjs.locale("pt-br");
 
 export function AttendeeList() {
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  const totalPages = Math.ceil(attendees.length / 10);
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  function goToNextPage() {
+    setPage(page + 1);
+  }
+
+  function goToPreviousPage() {
+    setPage(page - 1);
+  }
+
+  function goToLastPage() {
+    setPage(totalPages);
+  }
+
+  function goToFirstPage() {
+    setPage(1);
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -21,11 +54,14 @@ export function AttendeeList() {
         <div className="flex items-center gap-3 w-72 px-3 py-1.5 border-white/10 rounded-lg text-sm">
           <Search className="size-4 text-emerald-300" />
           <input
+            onChange={onSearchInputChanged}
             className="flex-1 bg-transparent outline-none border-0 p-0 text-sm"
             placeholder="Buscar participante"
           />
         </div>
       </div>
+
+      {search}
 
       <Table>
         <thead>
@@ -40,36 +76,33 @@ export function AttendeeList() {
             <TableHeader>Participante</TableHeader>
             <TableHeader>Data de inscrição</TableHeader>
             <TableHeader>Data do check-in</TableHeader>
-            <th
-              style={{ width: 64 }}
-              className="py-3 px-4 text-sm font-semibold text-left"
-            ></th>
+            <TableHeader style={{ width: 64 }}></TableHeader>
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
-              <TableRow key={i}>
+              <TableRow key={attendee.id}>
                 <TableCell>
                   <input
                     type="checkbox"
                     className="size-4 bg-black/20 rounded border border-white/10"
                   />
                 </TableCell>
-                <TableCell>12341</TableCell>
+                <TableCell>{attendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Wilker Guimarães
+                      {attendee.name}
                     </span>
-                    <span>wilker@gmail.com</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 dias atrás</TableCell>
-                <TableCell>3 dias atrás</TableCell>
+                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>
-                  <IconButton>
-                    <MoreHorizontal className="sizee-4" />
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
                   </IconButton>
                 </TableCell>
               </TableRow>
@@ -77,29 +110,41 @@ export function AttendeeList() {
           })}
         </tbody>
         <tfoot>
-          <TableCell colSpan={3}>Mostrando 10 de 228 items</TableCell>
-          <td
-            className="py-3 px-4 text-sm text-zinc-300 text-right"
-            colSpan={3}
-          >
-            <div className="inline-flex items-center gap-8">
-              <span>Página 1 de 23</span>
-              <div className="flex gap-1.5">
-                <IconButton>
-                  <ChevronsLeft className="size-4" />
-                </IconButton>
-                <IconButton>
-                  <ChevronLeft className="size-4" />
-                </IconButton>
-                <IconButton>
-                  <ChevronRight className="size-4" />
-                </IconButton>
-                <IconButton>
-                  <ChevronsRight className="size-4" />
-                </IconButton>
+          <tr>
+            <TableCell colSpan={3}>
+              Mostrando 10 de {attendees.length} items
+            </TableCell>
+            <td
+              className="py-3 px-4 text-sm text-zinc-300 text-right"
+              colSpan={3}
+            >
+              <div className="inline-flex items-center gap-8">
+                <span>
+                  Página {page} de {totalPages}
+                </span>
+                <div className="flex gap-1.5">
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
+                </div>
               </div>
-            </div>
-          </td>
+            </td>
+          </tr>
         </tfoot>
       </Table>
     </div>
